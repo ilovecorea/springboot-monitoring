96d6b5bb1c6f340833f51e1b864712c7bfafa7ca
heedong.kang <at6jiki3@12>
2021-10-16 19:23:29 +0900

    init

---
+++ spring-app/src/main/java/com/example/springapp/SpringAppController.java
@@ -0,0 +1,26 @@
+package com.example.springapp;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Random;
+
+@RestController
+public class SpringAppController {
+
+	@GetMapping("/")
+	public String hostname() throws UnknownHostException {
+		InetAddress addr = InetAddress.getLocalHost();
+		return addr.getHostName();
+	}
+
+	@GetMapping(value = "/test")
+	public ResponseEntity randomStatusTest() {
+		HttpStatus status = HttpStatus.values()[new Random().nextInt(HttpStatus.values().length)];
+		return new ResponseEntity(status);
+	}
+}
