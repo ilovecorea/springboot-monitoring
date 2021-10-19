ebf78349aabb14620e312d21ffb90cd01d8ffeac
heedong.kang <heedong.kang@lemonhc.com>
2021-10-19 11:41:33 +0900

    add consul

---
+++ spring-blog/src/test/java/com/example/blog/SpringBlogApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+class SpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
